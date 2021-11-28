e290ecf803fa587e9b3a92a02bf1acc552733d4d
Himanshu-sudo <himanshudhakate07@gmail.com>
2021-11-28 21:24:09 +0530

    Commited

---
+++ javascript/jsfile.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows : false , 
+         dots : true,
+         appendDots: '.slider-Dots',
+         dotsClass : 'dots'
+
+    });
+
+
+});
